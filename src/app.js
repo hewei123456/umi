78cb2ab5a55023b417ea38e5418acddfec3a0359
hewei <hewei95324@offcn.com>
2019-11-21 17:08:05 +0800

    添加persist

--- src/app.js
+++ src/app.js
@@ -1,8 +1,26 @@
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/es/storage/session';
+import immutableTransform from 'redux-persist-transform-immutable';
+
+const persistConfig = {
+  transforms: [immutableTransform()],
+  key: 'model',
+  storage,
+};
+const persistEnhancer = () => createStore => (reducer, initialState, enhancer) => {
+  const store = createStore(persistReducer(persistConfig, reducer), initialState, enhancer);
+  const persist = persistStore(store);
+  return { ...store, persist };
+};
+
 export const dva = {
   config: {
     onError(err) {
       err.preventDefault();
       console.error(err.message);
     },
+    extraEnhancers: [
+      persistEnhancer(),
+    ],
   },
 };
